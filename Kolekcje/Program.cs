3f038592001b410155d8501310129f95abaabdef
Krzysiek Wróblewski <krzysiuw1906@hotmail.com>
2019-09-19 18:08:45 +0200

    Add project files.

---
+++ Kolekcje/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names ={ { "Rafal", "Krzysiek" }, {"Rafal", "Krzysiek" } };
+
+            for (int i = 0; i < names.GetLength(0); i++)
+            {
+                for (int y = 0; y <2; y++)
+                {
+                    Console.WriteLine(names[i,y]);
+                }
+
+            }
+            //for (int i=0; i<names.Length; i++)
+            //{
+            //    for (int y = 0; y < names.Length; y++)
+            //    {
+            //        Console.WriteLine( names[i,y] ); 
+            //    }
+               
+            //}
+
+            Console.Read();
+
+        }
+    }
+}
